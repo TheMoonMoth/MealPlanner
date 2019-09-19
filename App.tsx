80b9b1fad857a144946b5e7da21066355100ddb3
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2019-09-18 21:25:54 -0600

    Added RecipeList to App

--- App.tsx
+++ App.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavBar } from './src/Header';
+import { RecipeList } from './src/components';
+import api from './src/api';
 
-export default function App() {
-  return (
-    <>
-      <View style={styles.headSlug} />
-      <View style={styles.container}>
-        <NavBar />
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-        <Text>More bullshit</Text>
-      </View>
-    </>
-  );
+export default class App extends React.Component {
+  state = {
+    recipes: [],
+    ingredients: [],
+    markets: [],
+  }
+  
+  async componentDidMount() {
+
+    const recipeData = api.getRecipes();
+    const ingredientData = api.getIngredients();
+    const marketData = api.getMarkets();
+
+    Promise.all([recipeData, ingredientData, marketData]).then((values) => {
+      this.setState({
+        recipes: values[0].recipes, // add type
+        ingredients: values[1].ingredients,
+        markets: values[2].markets,
+      })
+    });
+  }
+
+  render() {
+    const { recipes, ingredients, markets } = this.state;
+    console.warn('allData', recipes, ingredients, markets);
+    return (
+      <>
+        <View style={styles.headSlug} />
+        <View style={styles.container}>
+          <NavBar />
+          <RecipeList recipes={recipes}/>
+        </View>
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
