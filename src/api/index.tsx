e7f58727705b0c94003707823531658c28145e09
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2019-09-18 20:01:19 -0600

    initial GET api calls

---
+++ src/api/index.tsx
@@ -0,0 +1,9 @@
+import { getRecipes } from './recipeApi';
+import { getIngredients } from './ingredientApi';
+import { getMarkets } from './marketApi';
+
+export default {
+  getRecipes,
+  getIngredients,
+  getMarkets,
+};
---
+++ src/api/recipeApi.tsx
@@ -0,0 +1,19 @@
+const HOST = 'http://10.0.0.238:3000';
+
+export const getRecipes = async () => {
+  // TODO Redux network calls
+  try {
+    const res = await fetch(`${HOST}/recipes`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: null,
+    });
+    return res.json();
+  }
+  catch (err) {
+    return console.warn(err);
+  } 
+};
